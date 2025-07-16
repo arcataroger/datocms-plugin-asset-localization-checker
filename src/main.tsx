6b0291011bbc48c3205aff2717fdcb94a01de05b
Roger Tuan <r.tuan@datocms.com>
2025-07-16 10:13:49 -0700

    Version bumps & automatically enabled for file fields

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,16 @@ import { render } from "./utils/render";
 import { AssetLocalizationChecker } from "./components/AssetLocalizationChecker.tsx";
 
 connect({
-  manualFieldExtensions() {
-    return [
-      {
-        id: "assetLocalizationChecker",
-        name: "Asset Localization Checker",
-        type: "addon",
-        fieldTypes: ["file"],
-      },
-    ];
+  overrideFieldExtensions(field) {
+    if (field.attributes.field_type === "file") {
+      return {
+        addons: [
+          {
+            id: "assetLocalizationChecker",
+          },
+        ],
+      };
+    }
   },
   renderFieldExtension(id, ctx) {
     if (id === "assetLocalizationChecker") {
